c82ed647b8641057c188a3223d0caf601e502fab
Hernan Coronel <dam.hernan.coronel@gmail.com>
2025-10-07 18:58:00 +0200

    fix and new project

--- UD1/Regexps/Regexps/Form1.cs
+++ UD1/Regexps/Regexps/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 
@@ -21,7 +15,7 @@ namespace RegExps
         public Form1()
         {
             InitializeComponent();
-            TboxPaste.Leave += new System.EventHandler(this.TboxPaste_Leave);
+            TboxPaste.Leave += new EventHandler(this.TboxPaste_Leave);
         }
 
         private void BtnUrl_Click(object sender, EventArgs e)
