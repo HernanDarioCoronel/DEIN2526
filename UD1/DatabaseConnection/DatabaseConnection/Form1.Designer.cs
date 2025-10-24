da779b1290361d5e71838820b65b5b78b0f99dc0
Hernan Coronel <dam.hernan.coronel@gmail.com>
2025-10-24 19:43:23 +0200

    database

---
+++ UD1/DatabaseConnection/DatabaseConnection/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace DatabaseConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctIdCliente = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ctApel1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ctApel2 = new System.Windows.Forms.TextBox();
+            this.ctNome = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btModificar = new System.Windows.Forms.Button();
+            this.btEliminar = new System.Windows.Forms.Button();
+            this.btActualizar = new System.Windows.Forms.Button();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.btInsertar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ctIdCliente
+            // 
+            this.ctIdCliente.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ctIdCliente.Enabled = false;
+            this.ctIdCliente.Location = new System.Drawing.Point(3, 33);
+            this.ctIdCliente.Name = "ctIdCliente";
+            this.ctIdCliente.Size = new System.Drawing.Size(65, 20);
+            this.ctIdCliente.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LightSalmon;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(3, 16);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(2);
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Id Cliente";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightSalmon;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(2);
+            this.label1.Size = new System.Drawing.Size(165, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Apellido 1";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.LightSalmon;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(3, 16);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(2);
+            this.label2.Size = new System.Drawing.Size(198, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Apellido 2";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.LightSalmon;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(3, 16);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(2);
+            this.label3.Size = new System.Drawing.Size(162, 19);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Nome";
+            // 
+            // ctApel1
+            // 
+            this.ctApel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ctApel1.Location = new System.Drawing.Point(3, 32);
+            this.ctApel1.Name = "ctApel1";
+            this.ctApel1.Size = new System.Drawing.Size(165, 20);
+            this.ctApel1.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.ctIdCliente);
+            this.groupBox1.Location = new System.Drawing.Point(12, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(71, 56);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.ctApel1);
+            this.groupBox2.Location = new System.Drawing.Point(89, 9);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(171, 55);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.ctApel2);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Location = new System.Drawing.Point(266, 9);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(204, 56);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            // 
+            // ctApel2
+            // 
+            this.ctApel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ctApel2.Location = new System.Drawing.Point(3, 33);
+            this.ctApel2.Name = "ctApel2";
+            this.ctApel2.Size = new System.Drawing.Size(198, 20);
+            this.ctApel2.TabIndex = 15;
+            // 
+            // ctNome
+            // 
+            this.ctNome.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ctNome.Location = new System.Drawing.Point(3, 33);
+            this.ctNome.Name = "ctNome";
+            this.ctNome.Size = new System.Drawing.Size(162, 20);
+            this.ctNome.TabIndex = 16;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.ctNome);
+            this.groupBox4.Location = new System.Drawing.Point(473, 9);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(168, 56);
+            this.groupBox4.TabIndex = 17;
+            this.groupBox4.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(542, 173);
+            this.listBox1.TabIndex = 18;
+            // 
+            // btModificar
+            // 
+            this.btModificar.Location = new System.Drawing.Point(566, 158);
+            this.btModificar.Name = "btModificar";
+            this.btModificar.Size = new System.Drawing.Size(75, 23);
+            this.btModificar.TabIndex = 2;
+            this.btModificar.Text = "&Modificar";
+            this.btModificar.UseVisualStyleBackColor = true;
+            this.btModificar.Click += new System.EventHandler(this.btModificar_Click);
+            // 
+            // btEliminar
+            // 
+            this.btEliminar.Location = new System.Drawing.Point(566, 247);
+            this.btEliminar.Name = "btEliminar";
+            this.btEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btEliminar.TabIndex = 1;
+            this.btEliminar.Text = "&Eliminar";
+            this.btEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btActualizar
+            // 
+            this.btActualizar.Location = new System.Drawing.Point(566, 111);
+            this.btActualizar.Name = "btActualizar";
+            this.btActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btActualizar.TabIndex = 0;
+            this.btActualizar.Text = "&Actualizar Lista";
+            this.btActualizar.UseVisualStyleBackColor = true;
+            // 
+            // btSalir
+            // 
+            this.btSalir.Location = new System.Drawing.Point(650, 247);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(75, 23);
+            this.btSalir.TabIndex = 20;
+            this.btSalir.Text = "&Salir";
+            this.btSalir.UseVisualStyleBackColor = true;
+            this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+            // 
+            // btInsertar
+            // 
+            this.btInsertar.Location = new System.Drawing.Point(650, 25);
+            this.btInsertar.Name = "btInsertar";
+            this.btInsertar.Size = new System.Drawing.Size(75, 36);
+            this.btInsertar.TabIndex = 21;
+            this.btInsertar.Text = "&Insertar Registro";
+            this.btInsertar.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.LightSalmon;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(3, 16);
+            this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(2);
+            this.label5.Size = new System.Drawing.Size(542, 23);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Listado de Clientes";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.listBox1);
+            this.groupBox5.Controls.Add(this.label5);
+            this.groupBox5.Location = new System.Drawing.Point(12, 71);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(548, 216);
+            this.groupBox5.TabIndex = 23;
+            this.groupBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 299);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.btInsertar);
+            this.Controls.Add(this.btModificar);
+            this.Controls.Add(this.btEliminar);
+            this.Controls.Add(this.btSalir);
+            this.Controls.Add(this.btActualizar);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ctIdCliente;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ctApel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox ctApel2;
+        private System.Windows.Forms.TextBox ctNome;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btModificar;
+        private System.Windows.Forms.Button btEliminar;
+        private System.Windows.Forms.Button btActualizar;
+        private System.Windows.Forms.Button btSalir;
+        private System.Windows.Forms.Button btInsertar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox5;
+    }
+}
+
---
+++ UD1/DatabaseConnection/DatabaseConnection/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseConnection
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds;
+        OleDbConnection connection;
+        OleDbDataAdapter adapter;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string dbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Negocio.mdb");
+            string connectionString = $@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={dbPath};";
+
+            connection = new OleDbConnection(connectionString);
+
+
+            adapter = new OleDbDataAdapter("SELECT * FROM Clientes ORDER BY Apelido1, Apelido2;", connection);            ds = new DataSet();            adapter.Fill(ds, "Clientes");
+            this.RefreshList();
+        }
+
+        private void RefreshList()
+        {
+            listBox1.Items.Clear();
+
+            DataTable table = ds.Tables["Clientes"];
+
+            foreach (DataRow row in table.Rows)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(row["id"]);
+                sb.Append("-");
+                sb.Append(row["Apelido1"]);
+                sb.Append("-");
+                sb.Append(row["Apelido2"]);
+                sb.Append("-");
+                sb.Append(row["Nome"]);
+                sb.Append("-");
+                sb.Append(row["codigoProvincia"]);
+
+                listBox1.Items.Add(sb.ToString());
+            }
+        }
+
+        private void btModificar_Click(object sender, EventArgs e)
+        {
+            DataTable table = ds.Tables["Clientes"];
+
+            String[] selectedData = listBox1.SelectedItem.ToString().Split('-');
+            DataRow foudRow = null;
+            foreach (DataRow row in table.Rows)
+            {
+                if (row["id"].ToString().Equals(selectedData[0]))
+                {
+                    foudRow = row;
+                    break;
+                }
+            }
+            if (foudRow != null)
+            {
+                this.ctIdCliente.Text = foudRow["id"].ToString();
+                this.ctNome.Text = foudRow["Nome"].ToString();
+                this.ctApel1.Text = foudRow["Apelido1"].ToString();
+                this.ctApel2.Text = foudRow["Apelido2"].ToString();
+            }
+        }
+
+        private void btSalir_Click(object sender, EventArgs e)
+        {
+            connection.Close();
+            this.Close();
+        }
+    }
+}
