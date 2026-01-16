3146231b463573004995c7f612e67b28a4f3bacc
Hernan Coronel <dam.hernan.coronel@gmail.com>
2026-01-16 17:40:55 +0100

    no message

---
+++ UD2/DataBinding/DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CambiarModo(object sender, RoutedEventArgs e)
+        {
+            if (txt1Bnd == null) return;
+
+            RadioButton rb = sender as RadioButton;
+            if (rb == null) return;
+
+            Binding nuevoBinding = new Binding("Text");
+            nuevoBinding.ElementName = "txt2Bnd";
+
+            switch (rb.Content.ToString())
+            {
+                case "One Time": nuevoBinding.Mode = BindingMode.OneTime; break;
+                case "One Way": nuevoBinding.Mode = BindingMode.OneWay; break;
+                case "Two Way": nuevoBinding.Mode = BindingMode.TwoWay; break;
+                case "OneWay To Source": nuevoBinding.Mode = BindingMode.OneWayToSource; break;
+            }
+
+            txt1Bnd.SetBinding(TextBox.TextProperty, nuevoBinding);
+        }
+    }
+}
