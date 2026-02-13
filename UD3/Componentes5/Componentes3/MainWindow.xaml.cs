93a8808c40723f5f9fb2e13a5b1a22bfcf4a2292
IESPIRINGALLA\cor___her <dam.hernan.coronel@gmail.com>
2026-02-13 17:34:45 +0100

    arreglar colores

---
+++ UD3/Componentes5/Componentes3/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Componentes3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Color color = new Color();
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sliderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color.R = (byte)sliderR.Value;
+            sliderR.TextBoxColor = Color.FromRgb((byte)sliderR.Value, 0, 0);
+        }
+
+        private void sliderG_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color.G = (byte)sliderG.Value;
+            sliderG.TextBoxColor = Color.FromRgb(0, (byte)sliderG.Value, 0);
+        }
+
+        private void sliderB_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            color.B = (byte)sliderB.Value;
+            sliderB.TextBoxColor = Color.FromRgb(0, 0, (byte)sliderB.Value);
+        }
+    }
+}
---
+++ UD3/Componentes5/Input/LabeledSlider.xaml.cs
@@ -0,0 +1,74 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Input
+{
+    /// <summary>
+    /// Lógica de interacción para LabeledSlider.xaml
+    /// </summary>
+    public partial class LabeledSlider : UserControl
+    {
+        public LabeledSlider()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(int), typeof(LabeledSlider),
+                new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnValueChangedStatic));
+        public static readonly DependencyProperty MinValueProperty =
+            DependencyProperty.Register("MinValue", typeof(int), typeof(LabeledSlider), new PropertyMetadata(-100));
+        public static readonly DependencyProperty MaxValueProperty =
+            DependencyProperty.Register("MaxValue", typeof(int), typeof(LabeledSlider), new PropertyMetadata(100));
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string), typeof(LabeledSlider), new PropertyMetadata("Label"));
+        public static readonly DependencyProperty onValueChangeProperty =
+            DependencyProperty.Register("onValueChange", typeof(RoutedEventHandler), typeof(LabeledSlider), new PropertyMetadata(null));
+        public static readonly DependencyProperty TextBoxColorProperty =
+            DependencyProperty.Register("TextBoxColor", typeof(Color), typeof(LabeledSlider), new PropertyMetadata(Color.FromRgb(0,0,0)));
+        public static readonly RoutedEvent ValueChangedEvent =
+            EventManager.RegisterRoutedEvent("ValueChanged", RoutingStrategy.Bubble,
+                typeof(RoutedPropertyChangedEventHandler<double>), typeof(LabeledSlider));
+
+        private static void OnValueChangedStatic(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (LabeledSlider)d;
+            var args = new RoutedPropertyChangedEventArgs<double>((int)e.OldValue, (int)e.NewValue, ValueChangedEvent);
+            control.RaiseEvent(args);
+        }
+
+        public event RoutedPropertyChangedEventHandler<double> ValueChanged
+        {
+            add { AddHandler(ValueChangedEvent, value); }
+            remove { RemoveHandler(ValueChangedEvent, value); }
+        }
+
+        public int MinValue
+        {
+            get => (int)GetValue(MinValueProperty);
+            set => SetValue(MinValueProperty, value);
+        }
+        public int MaxValue
+        {
+            get => (int)GetValue(MaxValueProperty);
+            set => SetValue(MaxValueProperty, value);
+        }
+        public int Value
+        {
+            get => (int)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+        public Color TextBoxColor
+        {
+            get => (Color)GetValue(TextBoxColorProperty);
+            set => SetValue(TextBoxColorProperty, value);
+        }
+    }
+}
